947e643e363afac7a509a47278de0d9fd5db3955
henryaugusta <henryaugusta4@gmail.com>
2021-11-01 10:23:47 +0700

    Config Retrofit

---
+++ app/src/main/java/com/feylabs/rxjava_plain/ApiClient.kt
@@ -0,0 +1,57 @@
+package com.feylabs.rxjava_plain
+
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+object ApiClient {
+
+    fun provideLogger(): HttpLoggingInterceptor {
+        val logger = HttpLoggingInterceptor()
+
+        if (BuildConfig.DEBUG) {
+            // development build
+            logger.setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            // production build
+            logger.setLevel(HttpLoggingInterceptor.Level.BASIC)
+        }
+        return logger
+    }
+
+
+    fun provideClient(): OkHttpClient {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(provideLogger())
+            .addInterceptor(Interceptor { chain ->
+
+                val original: Request = chain.request()
+                val originalHttpUrl: HttpUrl = original.url
+                val url = originalHttpUrl.newBuilder()
+                    .addQueryParameter("api_key", "2671c64cf6ce8c1265cf8ce1c3543bf9")
+                    .build()
+                // Request customization: add request headers
+                val requestBuilder: Request.Builder = original.newBuilder()
+                    .url(url)
+                val request: Request = requestBuilder.build()
+                chain.proceed(request)
+            }).build()
+
+        return client
+    }
+
+    fun provideRetrofitClient(): Retrofit {
+        val client = Retrofit.Builder()
+            .client(provideClient())
+            .baseUrl(BuildConfig.BaseURL)
+            .build()
+        return client
+    }
+
+    fun provideGeneralService(): ApiService {
+        return provideRetrofitClient().create(ApiService::class.java)
+    }
+}
--- app/src/main/java/com/feylabs/rxjava_plain/ApiService.kt
+++ app/src/main/java/com/feylabs/rxjava_plain/ApiService.kt
@@ -1,6 +1,12 @@
 package com.feylabs.rxjava_plain
 
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+
 interface ApiService {
 
+    @GET("/movie/popular")
+    fun getMovies():
+            ResponseBody
 
 }
